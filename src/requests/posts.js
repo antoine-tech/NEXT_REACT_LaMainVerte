3ab3ea7fd16ac8ea1ae2c1001e3bba351bc3301e
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-11 14:57:15 +0100

    adding like dislike feature on post

--- src/requests/posts.js
+++ src/requests/posts.js
@@ -6,4 +6,30 @@ const getPosts = async () => {
   return response;
 };
 
-export { getPosts };
+const getPost = async (idPost) => {
+  const response = await API.find("/posts/" + idPost, false)
+    .then((res) => res.json())
+    .catch((error) => error);
+  return response;
+};
+
+const likePost = async (idPost, jwt_token) => {
+  const data = {
+    post_like: { post_id: idPost },
+  };
+  const response = await API.create(data, "/post_likes", true, jwt_token)
+    .then((res) => res.json())
+    .catch((error) => error);
+
+  return response;
+};
+
+const unlikePost = async (postLikeId, jwt_token) => {
+  const response = await API.delete("/post_likes/"+postLikeId, true, jwt_token)
+    .then((res) => res.text())
+    .catch((error) => error);
+
+  return response;
+};
+
+export { getPosts, getPost, likePost, unlikePost };
