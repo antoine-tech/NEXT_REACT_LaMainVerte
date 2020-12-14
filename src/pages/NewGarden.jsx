22dc25521b06c511585b429934196e75444d5c8f
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-14 11:19:10 +0100

    cropping backkground image

--- src/pages/NewGarden.jsx
+++ src/pages/NewGarden.jsx
@@ -6,15 +6,12 @@ import Alert from "../components/Alert/index";
 const NewGarden = () => {
   return (
     <section className="grid grid-cols-10">
-      <div
-        className={`hidden md:flex md:col-span-5 lg:col-span-6 items-center justify-center h-full bg-abstractform`}
-      >
+      <div className="hidden md:flex md:col-span-5 lg:col-span-6 items-center justify-center h-full bg-abstractform">
         <MyDropzone />
       </div>
 
-      <div className="grid grid-cols-1 col-span-10 md:col-span-5 lg:col-span-4 px-4 place-content-center min-h-screen overflow-y-auto relative">
+      <div className="col-span-10 md:col-span-5 lg:col-span-4 min-h-screen overflow-y-auto relative px-4">
         <GardenForm />
-       
       </div>
     </section>
   );
