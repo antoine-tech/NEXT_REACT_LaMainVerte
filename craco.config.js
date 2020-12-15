739c3ac99d3cdd7895cd21daaae7b9c0d233b835
Lamalle <julienlamalle63100@gmail.com>
2020-12-15 15:28:58 +0100

    commit whith mode on craco config

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,10 @@
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  mode: isDevelopment ? 'development' : 'production',
 };
