ec6f2851223f8d59d61a3c44b760ab48fb5c6124
Lamalle <julienlamalle63100@gmail.com>
2020-12-15 15:53:30 +0100

    commit without use craco

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,7 @@
-const isDevelopment = process.env.NODE_ENV !== 'production';
-
 module.exports = {
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
-  mode: isDevelopment ? 'development' : 'production',
 };
