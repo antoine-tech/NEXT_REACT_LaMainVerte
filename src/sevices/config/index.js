81b7a5680150465b6f03b2ee016666d77b951b3d
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-16 13:22:27 +0100

    removing outline

--- src/sevices/config/index.js
+++ src/sevices/config/index.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = `https://api-master-lamainverte.herokuapp.com/api`;
 
-const API_BASE_URL = `https://api-la-main-develop-qhww8s0b9m.herokuapp.com/api`;
+const API_BASE_URL = `https://api-la-main-develop-tizhrktwuy.herokuapp.com/api`;
 
 export default API_BASE_URL;
