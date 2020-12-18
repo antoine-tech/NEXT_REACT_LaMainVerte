0ba1052c8005179e313a225f0d765bbfded7ec10
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-18 10:16:25 +0100

    changing public profile

--- src/pages/PublicProfile.jsx
+++ src/pages/PublicProfile.jsx
@@ -22,19 +22,23 @@ const PublicProfile = () => {;
   return (
     <div className="Profile page">
       { user &&
-        <div>
-            <div className="flex flex-wrap justify-around">
-                <UserInformations user={user}/>
-                
-                <FollowedGardens user={user} />
-            </div>
-
-            <div className="flex flex-wrap justify-around">
-                <UserGardens user={user} />
-
-                <UserPosts user={user} />
-            </div>
-        </div>
+         <section className="grid grid-cols-12 min-h-80vh gap-4 bg-light-brown">
+         <div className="hidden md:block md:col-span-1 lg:col-span-2 bg-man relative"></div>
+         <div className="flex flex-col col-span-12 lg:col-span-10 grid grid-cols-2 gap-4 p-4">
+           <div className="col col-span-2 lg:col-span-1 lg-h-80vh h-screen overflow-auto">
+             <UserInformations user={user && user} />
+           </div>
+           <div className="col col-span-2 lg:col-span-1 lg-h-80vh h-screen overflow-auto">
+             <FollowedGardens user={user && user} />
+           </div>
+           <div className="col col-span-2 lg:col-span-1 lg-h-80vh h-screen overflow-auto">
+             <UserGardens user={user} />
+           </div>
+           <div className="col col-span-2 lg:col-span-1 lg-h-80vh h-screen verflow-auto">
+             <UserPosts user={user && user} />
+           </div>
+         </div>
+       </section>
       }
     </div>
   )
