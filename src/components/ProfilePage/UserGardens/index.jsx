ae33f3b3bb9249902e6cb9423ed2abd2f5e39dc8
antoine-tech <antoinehadrienleguillou@outlook.com>
2020-12-18 17:34:56 +0100

    adding empty illustation on allcontent profile user

--- src/components/ProfilePage/UserGardens/index.jsx
+++ src/components/ProfilePage/UserGardens/index.jsx
@@ -4,6 +4,7 @@ import { getFollowedGardenAndRelatedData } from "../../../requests/gardens";
 import { Link } from "react-router-dom";
 import GardenCard from "../../../components/GardenCard";
 import { useSelector } from "react-redux";
+import empty_result from "../../../assets/backgrounds/empty_gardens.svg";
 
 const UserGardens = ({ user }) => {
   const [gardens, setGardens] = useState([]);
@@ -39,6 +40,40 @@ const UserGardens = ({ user }) => {
           </>
         ) : (
           <h4>Jardins de {user.username}</h4>
+        )}
+      </div>
+
+      <div
+        className={
+          gardens?.length > 1 ? "gardens overflow-y-scroll" : "gardens"
+        }
+      >
+        {gardens.length > 0 ? (
+          gardens?.map((garden) => (
+            <GardenCard
+              key={`garden-${garden.id}`}
+              id={garden.id}
+              name={garden.name}
+              picture_url={garden.picture_url}
+              picture_opacity={garden.picture_opacity}
+              user={user}
+              climate={garden.climate}
+              location={garden.location}
+              garden_type={garden.garden_type}
+              created_at={garden.created_at}
+              updated_at={garden.updated_at}
+            />
+          ))
+        ) : (
+          <div className="flex  flex-col items-center w-full justify-center my-10">
+            <img
+              src={empty_result}
+              className="h-96 w-96"
+              alt="no result found"
+            />
+
+            <h4 className="my-4">Rien pour le moment ...</h4>
+          </div>
         )}
       </div>
 
